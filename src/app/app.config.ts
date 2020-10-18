899438399b6d8674d1cf269f09f355fd32c8d45f
Ahmed Abo Nahar <a.abonahar@gmail.com>
2020-10-19 01:36:38 +0200

    fix bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,126 +10,126 @@ export class Globals {
             projectNameAR: "دراسة جدوى مشروع منتجع سياحى",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/1.jpg",
-            location: ""
+            location: "egypt"
         },
         {
             id: "2",
-            projectNameAR: "دراسة جدوى مزرعة مشروع كوفي شوب عوائل",
+            projectNameAR: "دراسة جدوى مشروع كوفي شوب عوائل",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/2.jpg",
-            location: ""
+            location: "soudia"
         },
         {
             id: "3",
             projectNameAR: "دراسة جدوى صالون نسائى",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/3.jpg",
-            location: ""
+            location: "oman"
         },
         {
             id: "4",
             projectNameAR: "دراسة جدوى مشروع أدوات صحية",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/4.jpg",
-            location: ""
+            location: "kuwait"
         },
         {
             id: "5",
             projectNameAR: "دراسة جدوى فندق موسم الحج والعمرة",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/5.jpg",
-            location: ""
+            location: "egypt"
         },
         {
             id: "6",
             projectNameAR: "دراسة جدوى لإنشاء مستشفى",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/6.jpg",
-            location: ""
+            location: "qatar"
         },
         {
             id: "7",
             projectNameAR: "دراسة جدوى سيارة طعام",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/7.jpg",
-            location: ""
+            location: "kuwait"
         },        
         {
             id: "8",
             projectNameAR: "دراسة جدوى مزرعة دواجن",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/8.jpg",
-            location: ""
+            location: "soudia"
         },
         {
             id: "9",
             projectNameAR: "دراسة جدوى مكتب استيراد وتصدير",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/9.jpg",
-            location: ""
+            location: "qatar"
         },
         {
             id: "10",
             projectNameAR: "دراسة جدوى مركز صيانة",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/10.jpg",
-            location: ""
+            location: "egypt"
         },
         {
             id: "11",
             projectNameAR: "دراسة جدوى مصنع أدوات طبية",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/11.jpg",
-            location: ""
+            location: "emarat"
         },
          {
             id: "12",
             projectNameAR: "دراسة جدوى مصنع الإلكترونيات",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/12.jpg",
-            location: ""
+            location: "soudia"
         },
         {
             id: "13",
             projectNameAR: "دراسة جدوى مكتب عماله",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/13.jpg",
-            location: ""
+            location: "emarat"
         },
         {
             id: "14",
             projectNameAR: "دراسة جدوى مصنع أكواب بلاستيكيه",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/14.jpg",
-            location: ""
+            location: "oman"
         },
         {
             id: "15",
             projectNameAR: "دراسة جدوى شركة سفر وسياحة",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/15.jpg",
-            location: ""
+            location: "bahren"
         },
         {
             id: "16",
             projectNameAR: "دراسة جدوى مشروع فندق",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/16.jpg",
-            location: ""
+            location: "emarat"
         },
         {
             id: "17",
             projectNameAR: "دراسة جدوى مزرعة ومصنع ألبان",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/17.jpg",
-            location: ""
+            location: "egypt"
         },
         {
             id: "18",
             projectNameAR: "دراسة جدوى مصنع تغليف وتعبيئة",
             projectNameEN: "Lorem, ipsum dolor.",
             image: "../assets/img/18.jpg",
-            location: ""
+            location: "soudia"
         },
 
     ]
