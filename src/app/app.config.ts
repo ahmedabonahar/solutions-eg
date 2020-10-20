c0003e53e791d32a94211f4b3e70b20909c4d4e9
Ahmed Abo Nahar <a.abonahar@gmail.com>
2020-10-20 10:02:11 +0200

    add content fix bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,126 +8,126 @@ export class Globals {
         {
             id: "1",
             projectNameAR: "دراسة جدوى مشروع منتجع سياحى",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of a Resort.",
             image: "../assets/img/1.jpg",
             location: "egypt"
         },
         {
             id: "2",
             projectNameAR: "دراسة جدوى مشروع كوفي شوب عوائل",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of a family coffee shop.",
             image: "../assets/img/2.jpg",
             location: "soudia"
         },
         {
             id: "3",
             projectNameAR: "دراسة جدوى صالون نسائى",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of  women's/ Beauty salon.",
             image: "../assets/img/3.jpg",
             location: "oman"
         },
         {
             id: "4",
             projectNameAR: "دراسة جدوى مشروع أدوات صحية",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of sanitary ware.",
             image: "../assets/img/4.jpg",
             location: "kuwait"
         },
         {
             id: "5",
             projectNameAR: "دراسة جدوى فندق موسم الحج والعمرة",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of a hotel during Hajj and umrah season.",
             image: "../assets/img/5.jpg",
             location: "egypt"
         },
         {
             id: "6",
             projectNameAR: "دراسة جدوى لإنشاء مستشفى",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of hospital construction.",
             image: "../assets/img/6.jpg",
             location: "qatar"
         },
         {
             id: "7",
             projectNameAR: "دراسة جدوى سيارة طعام",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of a food truck.",
             image: "../assets/img/7.jpg",
             location: "kuwait"
         },        
         {
             id: "8",
             projectNameAR: "دراسة جدوى مزرعة دواجن",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of a poltury farm.",
             image: "../assets/img/8.jpg",
             location: "soudia"
         },
         {
             id: "9",
             projectNameAR: "دراسة جدوى مكتب استيراد وتصدير",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of import/export office.",
             image: "../assets/img/9.jpg",
             location: "qatar"
         },
         {
             id: "10",
             projectNameAR: "دراسة جدوى مركز صيانة",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of maintenance center.",
             image: "../assets/img/10.jpg",
             location: "egypt"
         },
         {
             id: "11",
             projectNameAR: "دراسة جدوى مصنع أدوات طبية",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of medical equipment factory.",
             image: "../assets/img/11.jpg",
             location: "emarat"
         },
          {
             id: "12",
             projectNameAR: "دراسة جدوى مصنع الإلكترونيات",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of electronic factory.",
             image: "../assets/img/12.jpg",
             location: "soudia"
         },
         {
             id: "13",
             projectNameAR: "دراسة جدوى مكتب عماله",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of employment agency.",
             image: "../assets/img/13.jpg",
             location: "emarat"
         },
         {
             id: "14",
             projectNameAR: "دراسة جدوى مصنع أكواب بلاستيكيه",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of plastic cup.",
             image: "../assets/img/14.jpg",
             location: "oman"
         },
         {
             id: "15",
             projectNameAR: "دراسة جدوى شركة سفر وسياحة",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of travel agency.",
             image: "../assets/img/15.jpg",
             location: "bahren"
         },
         {
             id: "16",
-            projectNameAR: "دراسة جدوى مشروع فندق",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameAR: "دراسة جدوى فندق",
+            projectNameEN: "Feasibility study of hotel construction.",
             image: "../assets/img/16.jpg",
             location: "emarat"
         },
         {
             id: "17",
             projectNameAR: "دراسة جدوى مزرعة ومصنع ألبان",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of farm dairy factory.",
             image: "../assets/img/17.jpg",
             location: "egypt"
         },
         {
             id: "18",
             projectNameAR: "دراسة جدوى مصنع تغليف وتعبيئة",
-            projectNameEN: "Lorem, ipsum dolor.",
+            projectNameEN: "Feasibility study of packing and packaging.",
             image: "../assets/img/18.jpg",
             location: "soudia"
         },
--- src/app/client/portfolio/portfolio.component.ts
+++ src/app/client/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Globals } from 'src/app/app.config';
 
 @Component({
@@ -9,12 +10,19 @@ import { Globals } from 'src/app/app.config';
 export class PortfolioComponent implements OnInit {
 
   data : any = [];
+  currentLang = localStorage.getItem('lang');
 
-  constructor(private global: Globals) {
+  constructor(private global: Globals, private translate: TranslateService,) {
     this.data = this.global.staticProjects;
+
+    translate.onLangChange.subscribe((val) => {
+      this.currentLang = translate.store.currentLang;
+    });
+
    }
 
   ngOnInit(): void {
   }
 
+  
 }
